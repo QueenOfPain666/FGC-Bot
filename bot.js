9dfd8a60c73c5ae2448d87b5e0a2ea78e12f8656
QueenOfPain666 <36765126+QueenOfPain666@users.noreply.github.com>
2018-03-09 13:35:32 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,19 @@
+/*
+ * Discord Bot
+ * Version 1.2.0
+ * QueenOfPain666
+ */
+
+const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
+
+if(!process.send) {
+
+Files.initStandalone();
+
+} else {
+
+process.on('message', function(content) {
+	Files.initBotTest(content);
+});
+
+}
